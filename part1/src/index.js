ad22d2de32f17d530e45c0651bb621f861da00dc
JH <764825656@qq.com>
2023-06-03 00:57:50 +0100

    exercise part1

---
+++ part1/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Courseinfo from './courseinfo'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Courseinfo/>)
