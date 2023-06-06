d67af9fbdc2fad07e4d856bcdbc0a40f0ef66f1f
JH <764825656@qq.com>
2023-06-06 22:06:15 +0100

    add course learning

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,6 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Courseinfo from './courseinfo'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
-
-ReactDOM.createRoot(document.getElementById('root')).render(<Courseinfo/>)
+ ReactDOM.createRoot(document.getElementById('root')).render(<Courseinfo/>)
