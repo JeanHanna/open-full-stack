7ef2256e84d2456adbd9b123149e1aafcfd362e2
JH <764825656@qq.com>
2023-06-12 16:12:39 +0100

    1.8: unicafe step3

--- part1/courseinfo/src/unicafe.js
+++ part1/courseinfo/src/unicafe.js
@@ -8,13 +8,28 @@ const Button = ({handleClick,text}) =>{
     )
 }
 
+const Statistics = ({good,neutral,bad}) => {
+    const all = good + neutral + bad
+    const avergae = (good - bad)/all
+    const positive = (good/all) * 100 
+    return(
+        <>
+        <h1>statistics</h1>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {avergae}</p>
+            <p>positive {positive}%</p>
+        </>
+    )
+}
+
 const Unicafe = () =>{
     const [good, setGood] = useState(0)
     const [neutral,setNeutral] = useState(0)
     const [bad, setBad] =useState(0)
-    const all = good + neutral + bad
-    const avergae = (good - bad)/all
-    const positive = (good/all) * 100 
+
 
 
     const handleGoodClick = () =>{
@@ -33,14 +48,7 @@ const Unicafe = () =>{
             <Button handleClick={handleGoodClick} text={'Good'}/>
             <Button handleClick={handleNeturalClick} text='Netural'/>
             <Button handleClick={handleBadClick } text='Bad'/>
-            <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {avergae}</p>
-            <p>positive {positive}%</p>
-
+            <Statistics good={good} bad={bad} neutral={neutral} />
         </div>
     )
 
