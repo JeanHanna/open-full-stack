1ecb1f44df7b715f9e77a9876dece8dfa0f8b4c6
JH <764825656@qq.com>
2023-06-18 22:20:59 +0100

    organize file

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Courseinfo from './courseinfo'
+import Unicafe from './unicafe'
+import Anecdotes from './anecdotes'
+
+//  ReactDOM.createRoot(document.getElementById('root')).render(<Courseinfo/>)
+//ReactDOM.createRoot(document.getElementById('root')).render(<Unicafe/>)
+ReactDOM.createRoot(document.getElementById('root')).render(<Anecdotes/>)
