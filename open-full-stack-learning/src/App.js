e6559e202dcd768746a7acdc405e436775d2f1dd
JH <764825656@qq.com>
2023-06-21 16:45:08 +0100

    part 2 rendering Collections

--- open-full-stack-learning/src/App.js
+++ open-full-stack-learning/src/App.js
@@ -1,50 +1,16 @@
-import {useState} from 'react'
-const History = (props) => {
-  // conditional rendering
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
+const App = (props) => {
+  const { notes } = props
+
   return (
     <div>
-      button press history: {props.allClicks.join(' ')}
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
     </div>
   )
 }
 
-const Button = ({handleClick,text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
-  console.log(allClicks)
-  return (
-    <div>
-      {left}
-      <Button handleClick={handleLeftClick} text = 'left' />
-      <Button handleClick={handleRightClick} text = 'right'/>
-      {right}
-      <History allClicks={allClicks} />
-    </div>
-  )
-}
 export default App
