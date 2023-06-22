1b3565670014f896a4d2b6f309d0bc2cffb917b5
JH <764825656@qq.com>
2023-06-22 22:23:42 +0100

    use reduce to calculate the total exercises

--- part2/coursecontents/src/components/Course.js
+++ part2/coursecontents/src/components/Course.js
@@ -12,12 +12,20 @@ const Content =({course}) =>{
     )
 }
 
+const Total =({parts}) =>{
+    const total = parts.reduce((sum,part) => sum + part.exercises , 0)
+    return(
+        <p>Total of {total} exercises</p>
+    )
+}
 const Course = ({course}) =>{
     console.log({course})
     return (
         <div>
             <Header course={course}/>
             <Content course={course}/>
+            <Total parts={course.parts}/>
+            
         </div>
     )
 }
