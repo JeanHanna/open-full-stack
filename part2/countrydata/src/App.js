3b3a1c7f199d293abcd571a125e975159bf4fc9a
JH <764825656@qq.com>
2023-06-28 23:04:17 +0100

    Rember! Don't render object! use an array instead!

--- part2/countrydata/src/App.js
+++ part2/countrydata/src/App.js
@@ -10,7 +10,8 @@ const App = () => {
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all')
     .then(response => {
-      setAllCountries(response.data)
+      setAllCountries(response.data.filter(country => country.name.common.toLowerCase().includes(searchCountry.toLowerCase())))
+      // console.log(response.data.name)
     })
     
   }, []);
--- part2/countrydata/src/components/Countries.js
+++ part2/countrydata/src/components/Countries.js
@@ -1,10 +1,12 @@
 // show searched country
 const Countries = ({allCountries,searchCountry}) => {
   console.log('alldata',allCountries,searchCountry)
+  const filtered = allCountries.filter(country => country.name.common.includes(searchCountry.toLowerCase()))
+  console.log(filtered)
+  // Rember! Don't render object! use an array instead!
     return(
         <ul>
-          {}
-        {/* {allCountries.filter(country => country.name.common.includes(searchCountry))} */}
+        {filtered.map(country => country.name.common)}
       </ul>
     )
 }
