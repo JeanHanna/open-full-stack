7e9e8704d9af04c683137df9e5e6e0fafe75d6b7
JH <764825656@qq.com>
2023-07-01 21:58:45 +0100

    ignore .env

--- part2/countrydata/src/components/Countries.js
+++ part2/countrydata/src/components/Countries.js
@@ -1,8 +1,28 @@
 import {useState,useEffect } from "react"
+import axios from "axios";
+
 
 const Countries = ({allCountries,searchCountry}) => {
+  const apiKey = process.env.API_KEY;
+  const [countries,setCountries] =useState([]);
+  const [capitalWeather,setCapitalWeather] = useState(null);
 
-  const [countries,setCountries] =useState([])
+  const getWeatherIconUrl = (iconCode) =>`https://openweathermap.org/img/wn/${iconCode}.png`;
+
+  const fetchCapitalWeather = async (capital) => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}`
+      );
+      setCapitalWeather(response.data);
+    } catch (error) {
+      console.error("Error fetching capital weather:", error);
+    }
+  };
+
+  console.log(capitalWeather)
+
+
 
   useEffect(() => {
     const filtered = allCountries.filter(country =>
@@ -11,14 +31,15 @@ const Countries = ({allCountries,searchCountry}) => {
     setCountries(filtered);
   }, [allCountries, searchCountry]);
 
-  console.log('alldata',allCountries,searchCountry)
-  const filtered = allCountries.filter(country => country.name.common.toLowerCase().includes(searchCountry.toLowerCase()))
-
+  useEffect(() => {
+    if (countries.length === 1) {
+      const capital = countries[0].capital[0]
+      fetchCapitalWeather(capital)
+    }
+  },[countries])
 
   if (searchCountry.length===0) {
-    return(
-      <p>please input a country name</p>
-    )
+    return <p>please input a country name</p>  
   }else if (countries.length === 1){
     const specificCountry = countries[0]
     const languageArray = Object.values(specificCountry.languages)
@@ -32,6 +53,17 @@ const Countries = ({allCountries,searchCountry}) => {
         {languageArray.map((language,index) => <li key={index}>{language}</li>)}
         </ul>
         <img src={specificCountry.flags.png} alt="Country Flag"></img>
+        {capitalWeather && (
+          <div>
+            <h2>Capital Weather</h2>
+            <p>Temperature: {(capitalWeather.main.temp - 273.15).toFixed(2)}°C</p>
+            <img
+              src={getWeatherIconUrl(capitalWeather.weather[0].icon)}
+              alt="Weather Icon"
+            />
+            <p>Wind: {capitalWeather.wind.speed} m/s</p>
+          </div>
+        )}
       </div>
     )
   }else if (1<countries.length && countries.length<=10 || countries.length===0){
