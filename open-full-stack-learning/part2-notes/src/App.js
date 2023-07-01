d751fed4fff6ee918920e5ef21fda4704dec3773
JH <764825656@qq.com>
2023-07-01 22:57:04 +0100

    button change the importance: use shallow copy&PUT

--- open-full-stack-learning/part2-notes/src/App.js
+++ open-full-stack-learning/part2-notes/src/App.js
@@ -52,6 +52,16 @@ const App = (props) => {
     : notes.filter(note => note.important)
   console.log(notesToShow)
 
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -62,7 +72,11 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note 
+          key={note.id} 
+          note={note}
+          toggleImportance={()=> toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
