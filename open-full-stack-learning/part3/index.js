7a06d49cab09aa2ce587a6a14a36815e7b89bc64
JH <764825656@qq.com>
2023-07-04 00:35:03 +0100

    middleware

--- open-full-stack-learning/part3/index.js
+++ open-full-stack-learning/part3/index.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const app = express()
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+app.use(requestLogger)
+
 //json-parser
 app.use(express.json())
 
@@ -74,7 +84,13 @@ let notes = [
   
     response.json(note)
   })
+// add middleware after routes This middleware will be used for catching requests made to non-existent routes   
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
   
+  app.use(unknownEndpoint)
+
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
