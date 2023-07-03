bb7efaaff1c9b4a69db2d226b0682fc9fac3daa1
JH <764825656@qq.com>
2023-07-03 17:20:24 +0100

    use Express fetching a resource acroadding REST

--- open-full-stack-learning/part3/index.js
+++ open-full-stack-learning/part3/index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [
     {
@@ -21,11 +22,23 @@ let notes = [
     }
   ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end(JSON.stringify(notes))
-})
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+  app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    console.log(id)
+    const note = notes.find(note => note.id === id)
+    console.log(note)
+    response.json(note)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
